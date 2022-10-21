9194851400a0200e9457f9486aa9f0e386133b7e
1DiegoDev <113364227+1DiegoDev@users.noreply.github.com>
2022-10-20 22:09:04 -0300

    Add files via upload

---
+++ js/jsglobal.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navegacaoId").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("navegacaoId").style.width = "0";
+  }
